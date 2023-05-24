885cfc8ae38d0d7facf5b228f61c2f42a6c308aa
Siraj Motaung <98824801+Rajmot@users.noreply.github.com>
2023-05-24 04:32:29 +0200

    Modified

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,9 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	char cha;
-	for (i = 0; i < 10; i++){
+
+	for (i = 0; i < 10; i++)
+	{
 		for (cha = 'a'; cha <= 'z'; cha++)
 			_putchar(cha);
 		_putchar('\n');
